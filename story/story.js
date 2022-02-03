b643f1505c92a28ac4108ba146b76d4978f4be46
wongpeggy <97936437+wongpeggy@users.noreply.github.com>
2022-02-02 20:38:29 -0700

    Update story.js

--- story/story.js
+++ story/story.js
@@ -39,7 +39,7 @@ function initMap() {
     const markerinfolist = [{
             lat: 22.3497609255,
             lng: 114.1939888133,
-            info: "<img src='img/fat_jong_temple.jpg'>"
+            info: "<img src='img/fat_jong_temple.jpg'><p style="font-weight: bold;">Temple Hill Fat Jong Temple</p>"
         },
         {
             lat: 22.3555093106,
